2d338910a01ab62967ecbb089db661f99b06d49a
Lewington-pitsos <lewingtonpitsos@gmail.com>
2023-01-19 14:10:26 +1100

    make the script run once every 24 rather than all the time

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+import argparse
 import random
 import logging
 import praw
 import time
 import json
+import datetime
+from praw import models
+import argparse
 
-with open("secrets.json") as f:
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dry", help="perform a dry-run (no actual comments posted)",
+                    action="store_true", default=False)
+args = parser.parse_args()
+
+with open("config.json") as f:
     data = json.load(f)
 
-DOG_WORDS = ["woof", "dog", "bark", "doggo", "pupper", "dogg", "maya", "who's a good girl?"]
-REPLIES = ["woof", "borf", "*pant pant pant*", "*wag wag*", "ruf", "arf", "woof woof", "whine"]
-MAX_COMMENTS_PER_DAY = 4
+TRIGGERS = data['triggers']
+REPLIES = data['replies']
+MAX_COMMENTS_PER_DAY = data['max_comments_per_day']
+MS_WAIT_BETWEEN_COMMENTS = data['ms_wait_between_comments']
+SUBREDDIT = data['subreddit']
+LAST_N = data['last_n']
 
+praw.Reddit._prepare_untrusted_prawcore
 
 # Replace YOUR_CLIENT_ID and YOUR_CLIENT_SECRET with your own API keys
 reddit = praw.Reddit(
@@ -18,38 +31,55 @@ reddit = praw.Reddit(
   client_secret=data['secret'], 
   username=data['username'],
   password=data['password'],
-  user_agent="maya_bot_woof_woof/1.0.0"
+  user_agent="maya_bot_woof_woof/2.0.0"
 )
 
 user_id = reddit.user.me().id
-subreddit = reddit.subreddit('atrioc')
+subreddit: models.SubredditHelper = reddit.subreddit(SUBREDDIT)
 
-def is_interesting(comment):
-  for word in DOG_WORDS:
+def is_relevant(comment, relevant_substrings):
+  for word in relevant_substrings:
     if word in comment.lower():
       return True
   return False
 
+def already_replied(comment: models.Comment, user_id):
+    comment.refresh()
+    for reply in comment.replies:
+        if reply.author.id == user_id:
+            return True
+    return False
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-logging.info("About to start listening to comment stream")
-counter = 0
-reset_time = time.time()
+logging.info("About to read comments")
+replies_today = 0
+yesterday = time.time() - 60*60*24
 
-for comment in subreddit.stream.comments(skip_existing=True):
-    if is_interesting(comment.body) and comment.author.id != user_id:
-        if counter >= MAX_COMMENTS_PER_DAY:
-            current_time = time.time()
-            if current_time - reset_time >= 86400:
-                counter = 0
-                reset_time = current_time
-            else:
-                continue
-        try:
-            comment.reply(random.choice(REPLIES))
-            logging.info("Replied to comment %s with ID %s", comment.body, comment.id)
-            counter += 1
-        except Exception as e:
-            logging.error("Failed to reply to comment with ID %s: %s", comment.id, e)
-    else:
-        logging.debug("Comment with ID %s is not interesting", comment.id)
+comment: models.Comment
+for comment in subreddit.comments(limit=LAST_N):
+    if comment.created_utc > yesterday:
+        logging.debug("Comment with ID %s posted recently", comment.id)
+        if replies_today < MAX_COMMENTS_PER_DAY and\
+            is_relevant(comment.body, TRIGGERS) and\
+            comment.author.id != user_id and\
+            not already_replied(comment, user_id):
+            try:
+                if replies_today > 0:        
+                    logging.debug("about to sleep for 1000 ms")
+                    time.sleep(MS_WAIT_BETWEEN_COMMENTS) # reddit doesn't allow multiple posts within 15 minutes from low karma bots
+                
+                reply = random.choice(REPLIES)
+                if not args.dry:
+                    comment.reply(reply)
+                    logging.info("Replied to comment %s, ID %s with %s", comment.body, comment.id, reply)
+                else:
+                    logging.info("Would have replied to comment %s, ID %s with %s, but this is a dry run so no comments were actually posted", comment.body, comment.id, reply)
+                    
+                
+                replies_today += 1
+
+            except Exception as e:
+                logging.error("Failed to reply to comment with ID %s: %s", comment.id, e)
+
+logging.info("finished replying to all comments")
